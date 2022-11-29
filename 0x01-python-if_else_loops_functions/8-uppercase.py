746d959f553256a4fe29ed11a6e7d121adfa568f
Kelvenbango <kelvenbango@gmail.com>
2022-11-29 15:11:00 -0800

    if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        if ord(str) >= ord(A):
+            result = chr(ord(str) - 32)
+        print(f"{result}", end = '')
+    print()
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number < 0:
+        return 10 - (number % 10)
+    else:
+        return number % 10
+
