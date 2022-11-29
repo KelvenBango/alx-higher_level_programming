3e17a2b99763dceefcc126221462c1f0ee2c289d
Kelvenbango <kelvenbango@gmail.com>
2022-11-29 15:57:50 -0800

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('e') and i != ord('q'):
-        print(f"{chr(i)}", end = '')
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
