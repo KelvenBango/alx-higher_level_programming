f5dab6c46576b71e3e6c913187076d6e9118545e
Kelvenbango <kelvenbango@gmail.com>
2022-11-28 16:36:46 -0800

    python, hello world

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+STR = "Holberton School"
+STR_3TIMES = STR * 3
+print(f"{STR_3TIMES}\n{STR[:9]}")
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+comparator = 'is better than'
+if_statement = 'If the implementation is'
+TheZenOfPython = f"""
+                 The Zen of Python, by Tim Peters
+                 Beautiful {comparator} ugly.
+                 Explicit {comparator} implicit.
+                 Simple {comparator} complex.
+                 Complex {comparator} complicated.
+                 Flat {comparator} nested.
+                 Sparse {comparator} dense.
+                 Readability counts.
+                 Special cases aren't special enough to break the rules.
+                 Although practicality beats purity.
+                 Errors should never pass silently.
+                 Unless explicitly silenced.
+                 In the face of ambiguity, refuse the temptation to guess.
+                 There should be one-- and preferably only one --obvious way to do it.
+                 Although that way may not be obvious at first unless you're Dutch.
+                 Now is better than never.
+                 Although never is often better than *right* now.
+                 If the implementation is hard to explain, it's a bad idea.
+                 If the implementation is easy to explain, it may be a good idea.
+                 Namespaces are one honking great idea -- let's do more of those!
+                 """
