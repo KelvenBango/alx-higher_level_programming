5f3d633b98df8e2657766eefe8fe199fa4471bbd
Kelvenbango <kelvenbango@gmail.com>
2022-11-29 15:37:20 -0800

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
+
     if ord(c) >= ord(a) and ord(c) <= ord(z):
-        return true
+        return True
     else:
-        return false
+        return False
