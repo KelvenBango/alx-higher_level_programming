a23def5abd9d2181d7cd936e9bfd7490550da171
Kelvenbango <kelvenbango@gmail.com>
2022-11-29 15:25:39 -0800

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,9 @@
 
 def print_last_digit(number):
     if number < 0:
-        print(f"{10 - (number % 10)}")
+        print(f"{10 - (number % 10)}", end="")
         return 10 - (number % 10)
     else:
-        print(f"{number % 10}")
+        print(f"{number % 10}", end="")
         return number % 10
 
