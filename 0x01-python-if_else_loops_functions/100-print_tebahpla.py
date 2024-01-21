e70d54ff494a4a3d8ab142b6f79e43bb0d979aec
Kelven Bango <Kelvenbango@gmail.com>
2024-01-21 13:12:55 -0800

    FEAT: new feature

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for alphabet in range(ord('z'), ord('a') - 1, -1):
+    if alphabet % 2 == 0:
+        difference = 0
+    else:
+        difference = 32
+    print(f'{chr(alphabet - difference)}', end='')
