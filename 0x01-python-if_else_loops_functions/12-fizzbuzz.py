ad87a9e5f58704be02d73bfaf82f5c61a725f43b
Kelven Bango <Kelvenbango@gmail.com>
2024-01-21 13:05:13 -0800

    FIX: remove blank line at end of file for Pycodestyle validation

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,6 @@
 def fizzbuzz():
     """
     Prints the numbers from 1 to 100 separeted by a space
-        
         * For multiples of three print 'Fizz'instead of the number
           and for multiples of five print 'Buzz'
         * For numbers which are multiples of both three and five print FizzBuzz
@@ -11,13 +10,11 @@ def fizzbuzz():
     """
 
     for number in range(1, 101):
-        
         if number % 3 == 0 and number % 5 == 0:
             print('FizzBuzz', end=' ')
         elif number % 3 == 0:
             print('Fizz', end=' ')
         elif number % 5 == 0:
             print('Buzz', end=' ')
-        else: 
+        else:
             print(f'{number}', end=' ')
-
