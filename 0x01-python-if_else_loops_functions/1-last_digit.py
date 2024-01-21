3645003446287d0fbe48ee360ac71ab88f59d34b
Kelven Bango <Kelvenbango@gmail.com>
2024-01-21 04:11:41 -0800

    FEAT: new feature

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+message = 'and is 0' if last_digit == 0 else ('and is greater than 5' if last_digit > 5 else 'and is less than 6 and not 0')
+print(f'Last digit of {number} is {last_digit} {message}')
