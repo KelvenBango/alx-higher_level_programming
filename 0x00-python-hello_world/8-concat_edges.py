cc564b55f9561eab2e87e8a8b6a0f7b62492eaf6
Kelven Bango <Kelvenbango@gmail.com>
2024-01-21 02:04:06 -0800

    FIX: removed extra space at the beginning

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 splited_string = str.split(',')
-print(f'{splited_string[2][:28]} {splited_string[2][69:73]} {splited_string[0][:6]}')
+print(f'{splited_string[2][1:28]} {splited_string[2][69:73]} {splited_string[0][:6]}')
