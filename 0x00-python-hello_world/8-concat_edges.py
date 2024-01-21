3bee3ba36419eb6999724abda16436b32a15004a
Kelven Bango <Kelvenbango@gmail.com>
2024-01-21 02:01:38 -0800

    FEAT: new feature

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+splited_string = str.split(',')
+print(f'{splited_string[2][:28]} {splited_string[2][69:73]} {splited_string[0][:6]}')
