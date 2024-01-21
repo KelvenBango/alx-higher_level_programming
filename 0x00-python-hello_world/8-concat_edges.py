03ea6fc2ca037a9abba3310658d5e66aabc1d6c8
Kelven Bango <Kelvenbango@gmail.com>
2024-01-21 02:12:19 -0800

    FIX: removed splited_string variable and reused the str variable

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-splited_string = str.split(',')
-print(f'{splited_string[2][1:28]} {splited_string[2][69:73]} {splited_string[0][:6]}')
+str = str[39:67] + str[107:112] + str[:6]
+print(f'{str}')
