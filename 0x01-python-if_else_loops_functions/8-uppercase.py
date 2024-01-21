b57ad673ab827bf564061b8898f74ddc2520f3ba
Kelven Bango <Kelvenbango@gmail.com>
2024-01-21 11:23:21 -0800

    FEAT: new feature

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for caracter in str:
+        if ord(caracter) >= 97 and ord(caracter) <= 122:
+            caracter = chr(ord(caracter) - 32)
+        print('{}'.format(caracter), end='')
+    print()
