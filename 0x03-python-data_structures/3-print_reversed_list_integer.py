0af198a09c5fdbdeb252acd491b290dfb74cfd04
Kelven Bango <Kelvenbango@gmail.com>
2024-01-23 13:20:32 -0800

    FIX: changed reverse logic

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    #my_list.reverse()
     if my_list:
-        for number in my_list:
+        for number in my_list[::-1]:
             print('{:d}'.format(number))
