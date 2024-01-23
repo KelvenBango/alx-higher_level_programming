f05a088c9d7c54d36299e6683b80d81d147077e2
Kelven Bango <Kelvenbango@gmail.com>
2024-01-23 14:24:32 -0800

    FEAT: new feature

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for item in range(len(matrix[row])):
+            if item != len(matrix[row]) - 1:
+                end_space = ''
+            else:
+                end_space = ''
+            print('{:d}'.format(matrix[row][item]), end=end_space)
+        print()
