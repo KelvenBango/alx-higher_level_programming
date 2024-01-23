c01e130104c853117b55ca3b813e9131d05dcf29
Kelven Bango <Kelvenbango@gmail.com>
2024-01-23 13:48:21 -0800

    FIX: pycodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,7 @@ def no_c(my_string):
     first_char_to_remove = 'c'
     second_char_to_remove = 'C'
 
-    for character in my_string:
-        if character != first_char_to_remove and character != second_char_to_remove:
-            new_string += character
+    for char in my_string:
+        if char != first_char_to_remove and char != second_char_to_remove:
+            new_string += char
     return new_string
