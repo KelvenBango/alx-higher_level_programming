5c5ffaadef995786b462060b336d8431134c4545
Kelven Bango <Kelvenbango@gmail.com>
2024-01-23 13:46:12 -0800

    FIX: considered a capital character to be removed too

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,10 @@
 
 def no_c(my_string):
     new_string = ''
-    char_to_remove = 'c'
+    first_char_to_remove = 'c'
+    second_char_to_remove = 'C'
 
     for character in my_string:
-        if character != char_to_remove:
+        if character != first_char_to_remove and character != second_char_to_remove:
             new_string += character
     return new_string
