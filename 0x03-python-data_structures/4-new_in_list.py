7fe8e297f0ff0317d7fc57a848fa07ae6f64bb27
Kelven Bango <Kelvenbango@gmail.com>
2024-01-23 13:28:12 -0800

    FEAT: new feature

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    copy_of_my_list = my_list.copy()
+
+    if idx < 0:
+        return copy_of_my_list
+    if idx >= len(my_list):
+        return copy_of_my_list
+    copy_of_my_list[idx] = element
+
+    return copy_of_my_list
