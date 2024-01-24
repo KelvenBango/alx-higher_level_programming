0a1eb03512cb3be13ced885302be2a0f9a68fdf6
Kelven Bango <Kelvenbango@gmail.com>
2024-01-24 06:30:36 -0800

    FIX: return None if the len is <=0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 def max_integer(my_list=[]):
     max_number = 0
 
-    if my_list:
+    if len(my_list) <= 0:
+        return None
+    else:
         for number in my_list:
             if number > max_number:
                 max_number = number
         return max_number
-    else:
-        return None
