579f5c9de2fa29f8f6b1db6dab8e9a7e01fbdd5e
Kelven Bango <Kelvenbango@gmail.com>
2024-01-24 05:33:25 -0800

    FIX: removed print statement

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,9 +6,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
 
     return new_tuple
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-print(add_tuple(tuple_a, tuple_b))
-print(add_tuple(tuple_a, (1,)))
-print(add_tuple(tuple_a, ()))
