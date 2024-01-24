3a800e6bb68117d9b1cd193cf3ee3749de47f987
Kelven Bango <Kelvenbango@gmail.com>
2024-01-24 06:14:13 -0800

    FIX: pycodestyle using PEP8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
     sentence_length = len(sentence)
-    first_character = sentence [0] if sentence_length != 0 else None
+    first_character = sentence[0] if sentence_length != 0 else None
 
-    return sentence_length, first_character  
+    return sentence_length, first_character
