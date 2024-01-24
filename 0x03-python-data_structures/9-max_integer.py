1825667a9e60eefed8af514509534608d96f21d0
Kelven Bango <Kelvenbango@gmail.com>
2024-01-24 06:44:37 -0800

    FIX: for loop range

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if len(my_list) <= 0:
         return None
     else:
-        for index in len(my_list):
+        for index in range(len(my_list)):
             if my_list[index] > max_number:
                 max_number = my_list[index]
         return max_number
