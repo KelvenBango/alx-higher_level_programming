b47dd3592269ef8e1f6d8c768a9b689f995191ee
Kelven Bango <Kelvenbango@gmail.com>
2024-01-30 12:19:44 -0800

    FIX: pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
-
     for index, item in enumerate(matrix):
         new_matrix[index] = list(map(lambda item: item**2, item))
     return new_matrix
