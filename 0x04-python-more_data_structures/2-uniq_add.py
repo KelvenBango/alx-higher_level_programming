36f8ba1cd518e32daa802e5658c1c24433fdfde0
Kelven Bango <Kelvenbango@gmail.com>
2024-01-30 13:42:34 -0800

    FIX: pycodestyle

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
     unique_list = set(my_list)
-    result = 0 
+    result = 0
     for item in unique_list:
         result += item
     return result
