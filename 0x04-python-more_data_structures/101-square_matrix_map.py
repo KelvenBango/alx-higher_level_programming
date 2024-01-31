b0dc0ef41decd6e637cbd25b176de7cf1a2b1406
Kelven Bango <Kelvenbango@gmail.com>
2024-01-31 14:01:02 -0800

    FIX: logic

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-
-    for index, item in enumerate(matrix):
-        new_matrix[index] = list(map(lambda item: item**2, item))
-    return new_matrix
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
