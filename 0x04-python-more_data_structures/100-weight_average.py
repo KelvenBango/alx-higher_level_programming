31eaf9dbf935e3d98af4e7c39b98f8cdc0f6b09b
Kelven Bango <Kelvenbango@gmail.com>
2024-01-31 13:53:16 -0800

    FIX: logic

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,7 +8,7 @@ def weight_average(my_list=[]):
     denominator = 0
 
     for tuple in my_list:
-        numerator = tuple[0] * tuple[1]
-        denominator = tuple[1]
+        numerator += tuple[0] * tuple[1]
+        denominator += tuple[1]
     
     return numerator / denominator
