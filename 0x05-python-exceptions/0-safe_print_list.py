3866b7743b653d08d979eaadcf033f353f8f2cd8
Kelven Bango <Kelvenbango@gmail.com>
2024-02-05 14:15:10 -0800

    FIX: pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print('{}'.format(my_list[index]), end='')
             count_items += 1
         except IndexError:
-            print('Index does not exist')
             break
     print('')
 
