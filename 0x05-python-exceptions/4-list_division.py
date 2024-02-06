5fb78a3c4a1ca8f4d425f9441883bb34ee2e4a3e
Kelven Bango <Kelvenbango@gmail.com>
2024-02-05 23:46:38 -0800

    FEAT: new feature

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for index in range(list_length):
+        try:
+            div = my_list_1[index] / my_list_2[index]
+        except TypeError:
+            print('Wrong Type')
+        except ZeroDivisionError:
+            print('Division by 0')
+            div = 0
+        except IndexError:
+            print('List index out of range')
+            div = 0
+        finally:
+            new_list.append(div)
+    return new_list
+
