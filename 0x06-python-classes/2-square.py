736b972c330c647bbb10c23cd3348bba8d2e5b37
Kelven Bango <Kelvenbango@gmail.com>
2024-02-07 15:15:22 -0800

    FIX: pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-Define a class Square
-"""
+"""Define a class Square."""
+
 
 class Square:
 
     def __init__(self, size=0):
-        """
-        Initialize a new Square.
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
