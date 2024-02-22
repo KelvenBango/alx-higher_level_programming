2030fc9fe99b24236ccf51f96744da326831de4d
Kelven Bango <Kelvenbango@gmail.com>
2024-02-22 12:35:44 -0800

    feat: new feature

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""1-my_list moodule
+This module provides a class MyList that inherits from List.
+
+Functions:
+    print_sorted(self): Prints the list, but sorted(ascending sort)
+"""
+
+class MyList(list):
+    """MyList class that inherits from list"""
+
+    def print_sorted(self):
+        """Print the list, but sorted(ascending sort)"""
+        print(sorted(self))
+
