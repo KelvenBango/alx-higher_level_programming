755c9d0a937b095c980c587af81eed66d3fd278d
Kelven Bango <Kelvenbango@gmail.com>
2024-02-22 13:46:45 -0800

    fix: added a type() to check is a class is subclass

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -21,7 +21,7 @@ def is_kind_of_class(obj, a_class):
         Otherwise - False
     """
 
-    if isinstance(obj, a_class) or issubclass(obj, a_class):
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
         return True
     else:
         return False
