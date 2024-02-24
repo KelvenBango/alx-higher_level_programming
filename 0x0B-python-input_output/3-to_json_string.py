e94a6612ca8bb0715e02ddb71ccc5a06497b09a6
Kelven Bango <Kelvenbango@gmail.com>
2024-02-24 13:47:26 -0800

    feat: new feature

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""Defines a function that returns the JSON representation of an object(string)"""
+
+
+def to_json_string(my_obj):
+    """Serializes object to JSON format
+
+    Args:
+        my_obj: The object to be serialized
+
+    Returns:
+        The JSON representation of an object
+    """
+
+    return json.dumps(my_obj)
