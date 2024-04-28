7ce95878e777a27f646392edf6850fd2b2a17735
Kelven Bango <Kelvenbango@gmail.com>
2024-04-27 17:07:27 -0700

    feature

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-   console.log('0');
-} else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a })[1];
-  console.log(second);
-}
+
+const args = process.argv
+  .slice(2)
+  .map(arg => Number.parseInt(arg))
+  .sort((a, b) => b - a);
+const val = args.length < 2 ? 0 : args[1];
+
+console.log(val);
